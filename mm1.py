131d6d2685acc003f12265471dd862be37381045
valenpy22 <valentina.campos.o@usach.cl>
2024-08-15 17:02:38 -0400

    feat: casi todo

--- mm1.py
+++ mm1.py
@@ -14,6 +14,9 @@ import numpy as np
 import getopt
 import sys
 
+# Configuración de la semilla para garantizar la reproducibilidad de los resultados
+#np.random.seed(0)
+
 # Configuración de parámetros de la simulación
 
 # Descripción: Función que se encarga de parsear los argumentos de la línea de comandos.
@@ -41,7 +44,12 @@ def parse_arguments():
     
     return arrival_rate, service_rate, end_time
 
+# Parseo de los argumentos de la línea de comandos
 arrival_rate, service_rate, end_time = parse_arguments()
+
+# Función para generar tiempos de eventos (arribo o salida) usando la distribución exponencial
+def generate_event_time(rate):
+    return -np.log(1.0 - np.random.uniform())/rate
 
 # Validación de los parámetros
 if arrival_rate is None or service_rate is None or end_time is None:
@@ -59,12 +67,12 @@ if arrival_rate >= service_rate:
 # Definición de variables de estado
 Ak = 0 # Tiempo de arribo del job k
 Sk = 0 # Tiempo de servicio del job k
-Qk = 0 # Suma de tiempos durante el cual la cola ha sido de largo k
 
 # Definición de otras variables
 Na = 0 # Número de clientes que han llegado
 Nd = 0 # Número de clientes que han salido del sistema
-t = 0 # Tiempo de simulación
+
+# Definición de otras variables
 queue = [] # Cola de clientes
 queuetime=0 # Tiempos de cola suma de k * Qk
 max_queue_length = 0 # Máximo largo de la cola
@@ -74,21 +82,13 @@ sum_Sk = 0 # Suma de tiempos de servicio
 sum_QN = {} # Suma de QN
 QT = end_time # Tiempo total en que la cola tiene el largo máximo
 last_time = 0 # Marca de tiempo del último evento
-arrival_times = [] # Lista de tiempos de arribo
-residence_times = [] # Tiempo de residencia
-
-# Generación de eventos de arribo
-def generate_event_time(rate):
-    return -np.log(1.0 - np.random.uniform())/rate
-
-next_arrival = 0
-end_simulation = end_time
 
 # Simulación de eventos discretos
-current_time = 0
-next_departure = float('inf')
+current_time = 0 # Tiempo actual en la simulación
+next_departure = float('inf') # Tiempo de la siguiente salida, inicializado en infinito
+next_arrival = 0 # Sigueiente arribo inicial 
+end_simulation = end_time # Tiempo de fin de la simulación
 
-contador = 1
 
 # Mientras no se cumpla la condición de término
 while current_time < end_simulation:
@@ -97,56 +97,38 @@ while current_time < end_simulation:
         current_time = next_arrival
         # Si el servidor no está ocupado
         if Ls == 0:
-            # Se actualiza el servidor a ocupado
-            Ls = 1
-            # Se genera un tiempo de servicio y se programa la nueva salida
-            Sk = generate_event_time(service_rate)
-            next_departure = current_time + Sk
-            # Se suma el tiempo de servicio
-            sum_Sk += Sk
-            residence_times.append(Sk)
-            print(f"Tiempo de arribo: {current_time}, Tiempo de salida: {next_departure}, Tiempo de residencia: {Sk}")
+            Ls = 1   # Se actualiza el servidor a ocupado
+            Sk = generate_event_time(service_rate) # Genera tiempo de servicio
+            next_departure = current_time + Sk   # Programa la siguiente salida
+            sum_Sk += Sk # Acumula el tiempo de servicio
             
-        # Si el servidor está ocupado
+        # Si el servidor está ocupado, el cliente se agrega a la cola
         else:
             partial_service_time = current_time - (next_departure - Sk)
             sum_Sk += partial_service_time
-            # Se agrega el tiempo a la cola
-            queue.append(current_time)
-            # Se actualiza el largo máximo de la cola
-            max_queue_length = max(max_queue_length, len(queue))
+            queue.append(current_time)  # Se agrega el tiempo a la cola
+            max_queue_length = max(max_queue_length, len(queue)) # Se actualiza el largo máximo de la cola
+
 
         # Se genera un nuevo tiempo de arribo
         Ak = generate_event_time(arrival_rate)
         next_arrival = current_time + Ak
-        Na += 1
+        Na += 1 # Incrementa el contador de arribos
 
     # Si el próximo evento es una salida
     else:
         current_time = next_departure
         if len(queue) > 0:
-            arrival_time = queue.pop(0)
-            # Tiempo total que el trabajo estuvo en el sistema
-            residence_time = current_time - arrival_time
-            residence_times.append(residence_time)
-            # Sumar tiempo de residencia al total
-            sum_Sk += residence_time
-            arrival_times.append(arrival_time)
-
-            print(f"Tiempo de arribo: {arrival_time}, Tiempo de salida: {current_time}, Tiempo de residencia: {residence_time}")
-            # Se saca el primer elemento de la cola
-            Qk += (current_time - arrival_time)
-            # Se genera un tiempo de servicio y se programa la nueva salida
-            Sk = generate_event_time(service_rate)
+            arrival_time = queue.pop(0) # Saca al primer cliente de la cola
+            sum_Sk += current_time - arrival_time  # Calcula y acumula el tiempo de residencia del cliente
+            Sk = generate_event_time(service_rate) # Genera tiempo de servicio para el siguiente cliente en la cola
             next_departure = current_time + Sk
 
         else:
-            # Se actualiza el servidor a desocupado
-            Ls = 0
-            # Se programa la próxima salida como infinito
-            next_departure = float('inf')
+            Ls = 0  # Se actualiza el servidor a desocupado
+            next_departure = float('inf') # Se programa la próxima salida como infinito
 
-        Nd += 1
+        Nd += 1  # Incrementa el contador de salidas
 
     # Se actualiza el tiempo de simulación
     current_time = min(next_arrival, next_departure)
@@ -154,27 +136,27 @@ while current_time < end_simulation:
     # Captura del tiempo transcurrido antes de actualizar current_time
     time_elapsed = current_time - last_time
 
-     #Calcular cuando la cola esta vacía
+    # Calcular cuando la cola esta vacía
     if(len(queue) == 0) :
         Q0 += time_elapsed
 
     # Se calcula la suma de k * Qk
     queuetime = queuetime +  len(queue) *  time_elapsed
 
-    #Tiempo de la cola con su largo máximo
-    #Si el largo de la cola no está en el diccionario, se agrega
+    # Tiempo de la cola con su largo máximo
+    # Si el largo de la cola no está en el diccionario, se agrega
     if len(queue) not in sum_QN:
         sum_QN[len(queue)] = 0  
     
     #Se suma el tiempo que la cola estuvo con largo k al diccionario
     sum_QN[len(queue)]=sum_QN[len(queue)]+(current_time - last_time)
 
-    last_time = current_time
-    contador += 1
+    last_time = current_time # Actualiza la marca de tiempo del último evento
 
-#Calcular cuando la cola esta vacía
+# Calcular cuando la cola esta vacía
 if(len(queue) == 0) :
     Q0 += end_simulation - last_time
+
 
 # MEDIDAS DE RENDIMIENTO
 #Utilización y largo promedio de la cola
@@ -182,14 +164,12 @@ if Q0> QT:
     U = 0
     E_X=0
 else:
-    U = 1 - (Q0 / QT)
+    U = 1 - (Q0 / QT) # Utilización calculada
 
-    E_X = queuetime / QT
+    E_X = queuetime / QT # Largo promedio de la cola
 
 # Tiempo promedio de residencia
 if Nd > 0:
-    print(f"la suma es : {sum_Sk}")
-    # es 1900
     E_S = sum_Sk / Nd
 else:
     E_S = 0
